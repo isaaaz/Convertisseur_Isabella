694aef06789d230007dc2e89cbefd312910a90e9
isabella <isabella@localhost>
2021-03-09 21:50:11 -0500

    Push

--- Isabella_convertisseur/src/application/ConvertisseurController.java
+++ Isabella_convertisseur/src/application/ConvertisseurController.java
@@ -88,72 +88,65 @@ public class ConvertisseurController implements Initializable{
     private Button butquit4;
 
     
-    // Pour les longueurs 
+    // Créer la liste des unités pour les longeurs
     
-    private ObservableList<String> listLong = (ObservableList<String>) FXCollections.observableArrayList("Pouce","Pied","Yard","Mile");
+    private ObservableList<String> listLong = (ObservableList<String>) FXCollections.observableArrayList("Pouce","Pied","Yard","Mile","Millimètre","Centimètre","Décimètre","Mètre", "Kilomètre");
     
-    double [] longueur = {39370.1, 3280.84, 1093.61, 0.621371};
-    
-    private ObservableList<String> listLong2 = (ObservableList<String>) FXCollections.observableArrayList("Millimètre","Centimètre","Décimètre","Mètre", "Kilomètre");
-    
-    double [] longueur2 = {1000000.0, 100000.0, 10000.0, 1000.0, 1.0};
+    double [] longueur = {39370.1, 3280.84, 1093.61, 0.621371,1000000.0, 100000.0, 10000.0, 1000.0, 1.0};
     
 
-    // Pour les volumes
+    // Créer la liste des unités pour les volumes
     
-    private ObservableList<String> listVol = (ObservableList<String>) FXCollections.observableArrayList("Tasse","Pinte","Quart", "Gallon");
+    private ObservableList<String> listVol = (ObservableList<String>) FXCollections.observableArrayList("Tasse","Pinte","Quart", "Gallon","Millilitre","Litre");
     
-    double [] volume = {3.5195 ,1.7597, 0.879877, 0.219969};
+    double [] volume = {3.5195, 1.7597, 0.879877, 0.219969, 1000.0, 1.0};
+
     
-    private ObservableList<String> listVol2 = (ObservableList<String>) FXCollections.observableArrayList("Millilitre","Litre");
-    
-    double [] volume2 = {1000.0, 1.0};
-    
-    
-    // Pour le temps 
+    // Créer la liste des unités pour le temps 
     
     private ObservableList<String> listTemps = (ObservableList<String>) FXCollections.observableArrayList("Seconde", "Minute","Heure");
     
     double [] temps = {3600.0 ,60.0, 1.0};
 
     
-    // Pour les masses
+    // Créer la liste des unités pour les masses
     
-    private ObservableList<String> listMasse = (ObservableList<String>) FXCollections.observableArrayList("Once","Livre");
+    private ObservableList<String> listMasse = (ObservableList<String>) FXCollections.observableArrayList("Once","Livre","Milligramme","Gramme","Kilogramme");
     
-    double [] masse = {35.274, 2.20462};
-    
-    private ObservableList<String> listMasse2 = (ObservableList<String>) FXCollections.observableArrayList("Milligramme","Gramme","Kilogramme");
-    
-    double [] masse2 = {1000000, 1000, 1};
+    double [] masse = {35.274, 2.20462, 1000000.0, 1000.0, 1.0};
+   
      
     
     
     public void initialize(URL location, ResourceBundle resources)
     {
     	// Créer les comboBox pour les longueurs
+    	
     	cbolong1.setItems(listLong);
-    	cbolong2.setItems(listLong2);
+    	cbolong2.setItems(listLong);
     	cbolong1.getSelectionModel().selectFirst();
-    	cbolong2.getSelectionModel().selectFirst();
+    	cbolong2.getSelectionModel().select(4);
     	
     	// Créer les comboBox pour les volumes
+    	
     	cbovol1.setItems(listVol);
-    	cbovol2.setItems(listVol2);
+    	cbovol2.setItems(listVol);
     	cbovol1.getSelectionModel().selectFirst();
-    	cbovol2.getSelectionModel().selectFirst();
+    	cbovol2.getSelectionModel().select(4);
     	
     	// Créer les comboBox pour le temps
+    	
     	cbotemps1.setItems(listTemps);
     	cbotemps2.setItems(listTemps);
     	cbotemps1.getSelectionModel().selectFirst();
-    	cbotemps2.getSelectionModel().selectFirst();
+    	cbotemps2.getSelectionModel().select(1);
     	
     	// Créer les comboBox pour les masses
+    	
     	cbomasse1.setItems(listMasse);
-    	cbomasse2.setItems(listMasse2);
+    	cbomasse2.setItems(listMasse);
     	cbomasse1.getSelectionModel().selectFirst();
-    	cbomasse2.getSelectionModel().selectFirst();
+    	cbomasse2.getSelectionModel().select(2);
     	
     }
     
@@ -163,13 +156,13 @@ public class ConvertisseurController implements Initializable{
     @FXML
     void calLong1()
     {
-    	convertir(txtlong1,txtlong2,cbolong1,cbolong2,longueur,longueur2);
+    	convertir(txtlong1,txtlong2,cbolong1,cbolong2,longueur);
     }
     
 	@FXML
 	void calLong2()
 	{
-		convertir(txtlong2,txtlong1,cbolong2,cbolong1,longueur2,longueur);
+		convertir(txtlong2,txtlong1,cbolong2,cbolong1,longueur);
 	}
 	
 	
@@ -178,13 +171,13 @@ public class ConvertisseurController implements Initializable{
     @FXML
     void calVol1()
     {
-    	convertir(txtvol1,txtvol2,cbovol1,cbovol2,volume,volume2);
+    	convertir(txtvol1,txtvol2,cbovol1,cbovol2,volume);
     }
 
 	@FXML
 	void calVol2()
 	{
-		convertir(txtvol2,txtvol1,cbovol2,cbovol1,volume2,volume);
+		convertir(txtvol2,txtvol1,cbovol2,cbovol1,volume);
 	}
 	
 	
@@ -193,13 +186,13 @@ public class ConvertisseurController implements Initializable{
     @FXML
     void calTemps()
     {
-    	convertirSingle(txttemps1,txttemps2,cbotemps1,cbotemps2,temps);
+    	convertir(txttemps1,txttemps2,cbotemps1,cbotemps2,temps);
     }
     
     @FXML
     void calTemps2()
     {
-    	convertirSingle(txttemps2,txttemps1,cbotemps2,cbotemps1,temps);
+    	convertir(txttemps2,txttemps1,cbotemps2,cbotemps1,temps);
     }
 	
 	
@@ -209,36 +202,20 @@ public class ConvertisseurController implements Initializable{
     @FXML
     void calMasse1()
     {
-    	convertir(txtmasse1,txtmasse2,cbomasse1,cbomasse2,masse,masse2);
+    	convertir(txtmasse1,txtmasse2,cbomasse1,cbomasse2,masse);
     }
 
 	@FXML
 	void calMasse2()
 	{
-		convertir(txtmasse2,txtmasse1,cbomasse2,cbomasse1,masse2,masse);
+		convertir(txtmasse2,txtmasse1,cbomasse2,cbomasse1,masse);
 	}
 	
-	
-	
-	
-	// Méthode pour convertir d'unités d'un système à l'autre 
 
-	public void convertir(TextField txtA,TextField txtB,ComboBox boxA, ComboBox boxB, double [] tab, double [] tab2) 
-	{
-		verifNum(txtA);
-		
-		int item1 = boxA.getSelectionModel().getSelectedIndex();
-		int item2 = boxB.getSelectionModel().getSelectedIndex();
-		double taux = tab2[item2]/tab[item1];
-		double res = taux*(Double.parseDouble(txtA.getText()));
-		txtB.setText(Double.toString(res));
-		
-	}
 	
+	// Méthode pour convertir d'unités
 	
-	// Méthode pour convertir d'unités dans un seul système
-	
-	public void convertirSingle (TextField txtA,TextField txtB,ComboBox boxA, ComboBox boxB, double [] tab) 
+	public void convertir (TextField txtA,TextField txtB,ComboBox boxA, ComboBox boxB, double [] tab) 
 	{
 		verifNum(txtA);
 		
@@ -250,6 +227,7 @@ public class ConvertisseurController implements Initializable{
 	}
 	
 	
+
 	// Pour le système accepte des caractères numériques seulement 
 	
 	public void verifNum(TextField a)
@@ -263,6 +241,7 @@ public class ConvertisseurController implements Initializable{
 			}
 		});
 	}
+	
 	
 
 	// Pour quitter l'application
